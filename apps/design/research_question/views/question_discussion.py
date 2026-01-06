9281bf81d20a32e71b01e98599a29956050f9d69
Cristian Zambrano <94475992+Cristian-Zambrano@users.noreply.github.com>
2026-01-05 21:53:49 -0500

    Design refactor on logic and structure (#39)
    * refactor: selectors, policies and dtos for design
    
    * refactor: methods, services, dtos and selector to follow SOLID
    
    * refactor: methods, services, dtos and selector to follow SOLID in research questions
    
    * refactor: methods, services, dtos and selector to follow SOLID in eligibility criteria
    
    * refactor: methods, services, dtos and selector to follow SOLID in search strategy
    
    * fix(extraction): fix en la relacion de extraccion
    
    * fix: bugs tests
    
    ---------
    
    Co-authored-by: Cristian Zambrano <CZambrano10@slb.com>

--- apps/design/research_question/views/question_discussion.py
+++ apps/design/research_question/views/question_discussion.py
@@ -5,7 +5,9 @@ from django.contrib import messages
 
 from apps.project.decorators import project_member_required, build_design_url
 from apps.design.research_question.services.question_services import ResearchQuestionService
+from apps.design.research_question.selectors import ResearchQuestionSelector
 from apps.design.design_phase_logic.services.design_phase_service import DesignPhaseService
+from apps.design.design_phase_logic.selectors import DesignPhaseSelector
 from apps.design.design_phase_logic.models.design_phase import DesignPhase
 
 research_question_service = ResearchQuestionService()
@@ -16,9 +18,9 @@ design_phase_service = DesignPhaseService()
 def question_discussion_panel_view(request, project_id, project):
     status_filter = request.GET.get('status')
 
-    questions = research_question_service.get_discussion_research_questions_by_project(project_id, status_filter=status_filter)
-    current_stage_plan = design_phase_service.get_current_stage_plan(project_id)
-    timeline_stages = design_phase_service.get_design_timeline_context(project_id)
+    questions = ResearchQuestionSelector.get_list_for_discussion(project_id, request.user, status_filter=status_filter)
+    current_stage_plan = DesignPhaseSelector.get_current_stage_plan(project_id)
+    timeline_stages = DesignPhaseSelector.get_design_timeline_context(project_id)
 
     context = {
         'project': project,
